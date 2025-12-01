1f75c64605b65d7ee2b2f029c45c5c160bc79c6e
Richard-CRT <richard-crt@users.noreply.github.com>
2025-12-01 20:25:51 +0000

    Day 1 complete

---
+++ Day_01_Secret_Entrance/Program.cs
@@ -0,0 +1,81 @@
+﻿using AdventOfCodeUtilities;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.Marshalling;
+using System.Security;
+using System.Text.RegularExpressions;
+
+List<string> inputList = AoC.GetInputLines();
+
+
+void P1()
+{
+    int val = 50;
+    int result = 0;
+    foreach (var line in inputList)
+    {
+        char direction = line[0];
+        int inc = int.Parse(line[1..]);
+        inc = inc % 100;
+        if (direction == 'L')
+            val = (val - inc);
+        else
+            val = (val + inc);
+        if (val < 0)
+            val += 100;
+        else if (val >= 100)
+            val -= 100;
+
+        if (val == 0)
+            result++;
+    }
+
+    Console.WriteLine(result);
+    Console.ReadLine();
+}
+
+void P2()
+{
+    int val = 50;
+    int result = 0;
+    foreach (var line in inputList)
+    {
+        char direction = line[0];
+        int inc = int.Parse(line[1..]);
+
+        result += inc / 100;
+        inc = inc % 100;
+        
+        if (inc > 0)
+        {
+            if (direction == 'R')
+            {
+                val += inc;
+                if (val >= 100)
+                {
+                    val -= 100;
+                    result++;
+                }
+            }
+            else
+            {
+                int newVal = val - inc;
+                if (newVal == 0)
+                    result++;
+                else if (newVal < 0)
+                {
+                    if (val != 0)
+                        result++;
+                    newVal += 100;
+                    
+                }
+                val = newVal;
+            }
+        }
+    }
+
+    Console.WriteLine(result);
+    Console.ReadLine();
+}
+
+P1();
+P2();
